d8150a7e724a091c0ee7a112b4e73aebba7d256d
Victor Alves <144241751+victorbalves@users.noreply.github.com>
2024-03-07 22:34:26 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+    if(window.scrollY >= 500) {
+       cuca.style.background = "#191919" 
+    } else {
+        cuca.style.background = "transparent"
+    }
+})
+
+
